cc788c056bf08ee2bd902ca3a39bff44f10497bb
LaMik <lam.michal.franc@gmail.com>
2013-03-24 16:45:12 +0100

    modified shrinkwrap, added nano module used as a connector to coucdb, simple endpoint /actualfeeds

--- src/mainNode.js
+++ src/mainNode.js
@@ -1,12 +1,28 @@
  var appPortNumber = 12345;
  var mainDirectoryPath = __dirname;
 
+ var dbServer = require('nano')('http://localhost:8000');
+ var db =  dbServer.use('dotnetblogs');
+
  var express = require("express");
     var app = express();
      
     /* serves main page */
     app.get("/", function(req, res) {
         res.sendfile('index.htm');
+    });
+
+    /* return list of feeds */
+    app.get("/actualfeeds", function(req, res) {
+
+        var feeds = {};
+
+        db.view('list','feeds', function(err, body) {
+          if (!err) {
+              feeds = body.rows[0];
+              res.send(feeds);
+          }
+        });
     });
 
     /* serves all the static files */
