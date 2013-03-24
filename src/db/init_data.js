4f3952e6573cbed36e08b6cefcdd0bd27dcd8696
LaMik <lam.michal.franc@gmail.com>
2013-03-24 22:25:22 +0100

    adding node app to restore db with sample feeds and one view

---
+++ src/db/init_data.js
@@ -0,0 +1,96 @@
+  var feeds = [
+   "http://feeds.feedburner.com/jakubg",
+   "http://feeds.feedburner.com/maciejaniserowicz",
+   "http://maciej-progtech.blogspot.com/feeds/posts/default?alt=rss",
+   "http://feeds2.feedburner.com/Piotrosz",
+   "http://feeds.feedburner.com/devblogi",
+   "http://feeds.feedburner.com/szymonpobiega",
+   "http://geekswithblogs.net/kobush/Rss.aspx",
+   "http://feeds.feedburner.com/xion",
+   "http://marcinborecki.pl/feed/",
+   "http://teamsystem.pl/syndication.axd",
+   "http://feeds.feedburner.com/dario-g",
+   "http://jacekciereszko.pl/feeds/posts/default?alt=rss",
+   "http://michalkomorowski.blogspot.com/feeds/posts/default?alt=rss",
+   "http://feeds.feedburner.com/BartekSzafko",
+   "http://feeds2.feedburner.com/blogspot/wBGD",
+   "http://vbamania.blogspot.com/feeds/posts/default?alt=rss",
+   "http://machal85.wordpress.com/feed/",
+   "http://feeds2.feedburner.com/eastgroup",
+   "http://feeds.feedburner.com/LukaszGasior-Blog",
+   "http://feeds.feedburner.com/tomaszwisniewski",
+   "http://feeds.feedburner.com/kozmic",
+   "http://www.codeguru.pl/RSS/News.aspx",
+   "http://feeds2.feedburner.com/Dotnetomaniakpl-OstatnioOpublikowaneArtykuy",
+   "http://crmblog.pl/feed/",
+   "http://feeds.feedburner.com/MarcinWolanski",
+   "http://feeds.feedburner.com/sharepointblog_pl",
+   "http://feeds.feedburner.com/andrzejnetpl",
+   "http://feeds.feedburner.com/DariuszTarczynski",
+   "http://feeds.feedburner.com/JakubFlorczyk",
+   "http://feeds.feedburner.com/ProgramowanieIOkolice",
+   "http://www.pzielinski.com/?feed=rss2",
+   "http://www.lesnikowski.com/blog/index.php/feed/",
+   "http://blog.domas.pl/feed/",
+   "http://blog.poslinski.net/feed/",
+   "http://jlfedra.blogspot.com/feeds/posts/default?alt=rss",
+   "http://feeds2.feedburner.com/czoper",
+   "http://maciejgrabek.com/feed/",
+   "http://netmajor.wordpress.com/feed/",
+   "http://marcinkruszynski.blogspot.com/feeds/posts/default?alt=rss",
+   "http://revis-dev.blogspot.com/feeds/posts/default",
+   "http://feeds.feedburner.com/ZawodProgramistaNet",
+   "http://feeds.feedburner.com/michalnajman",
+   "http://poniat.wordpress.com/feed/",
+   "http://mmulawa.blogspot.com/feeds/posts/default",
+   "http://piotrsowa.eu/feed/",
+   "http://feeds.feedburner.com/office365blogpl",
+   "http://zine.net.pl/blogs/mgrzeg/rss.aspx",
+   "http://krzysztofmorcinek.wordpress.com/feed/",
+   "http://templinadam.wordpress.com/feed/",
+   "http://mndevnotes.wordpress.com/feed/",
+   "http://feeds.feedburner.com/PassionateProgram",
+   "http://pawel.sawicz.eu/feed/",
+   "http://paskol.robi.to/?feed=rss2"
+];
+
+var nano = require('nano')('http://localhost:8000');
+
+nano.db.destroy('dotnetblogs', function() {
+  // create a new database
+	nano.db.create('dotnetblogs', function() {
+    	// specify the database we are going to use
+    	var db = nano.use('dotnetblogs');
+
+    	// create feeds data
+    	db.insert({ list : feeds }, 'feeds', function(err, body, header) {
+      	if (err) {
+        	console.log('error inserting feeds', err.message);
+        	return;
+      	}
+      	console.log('restore finished');
+	    });
+
+	    // create design view for feeds
+
+	     db.insert(
+		  { "views": 
+		    { "feeds": 
+		      { 
+		      	"map": function(doc) {
+				  	var id = doc._id;
+				  	var feedList = doc.list;
+				  	emit(id, feedList);
+				}
+		      } 
+		    }
+		  }, '_design/list', function (error, response) {
+		  });
+    });
+});
+
+/*function(doc) {
+  var id = doc._id;
+  var feedList = doc.feedList;
+  emit(id, feedList);
+}*/
