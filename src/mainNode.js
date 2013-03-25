72ffbd8b40d579469638cb0129098be97cb43f28
LaMik <lam.michal.franc@gmail.com>
2013-03-25 21:50:54 +0100

    - moved user voice script to separate file

--- src/mainNode.js
+++ src/mainNode.js
@@ -6,13 +6,13 @@
 
  var express = require("express");
     var app = express();
-     
+
     /* serves main page */
     app.get("/", function(req, res) {
         res.sendfile('index.htm');
     });
 
-    /* return list of feeds */
+    /* return l1ist of feeds */
     app.get("/actualfeeds", function(req, res) {
 
         var feeds = {};
