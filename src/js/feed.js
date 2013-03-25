7c35237c8f9751dbebee6884d23bad4b5055c9db
LaMik <lam.michal.franc@gmail.com>
2013-03-25 20:14:27 +0100

    moved js logic to feed.js

---
+++ src/js/feed.js
@@ -0,0 +1,130 @@
+google.load('feeds', '1');
+
+        var testFeeds = [];
+
+        var data = {
+            feeds: new Array()
+        };
+
+        var toolTipIndex = 0;
+        
+        function allDone() {
+            toolTipIndex = 0;
+
+            doSort();
+            
+            $("#headlines").setTemplateElement("allFeeds");
+            $("#headlines").processTemplate(data);
+
+            $('.feed-block').each(function() {
+
+                toolTipIndex++;
+                if (toolTipIndex % 3 == 0) {
+                    cornerToolTip = 'topRight';
+                    cornerTarget = 'bottomLeft';
+                } else {
+                    cornerToolTip = 'topLeft';
+                    cornerTarget = 'bottomRight';
+                }
+
+                $(this).find('.hentry').each(function() {
+
+                    $(this).qtip(
+                    {
+                        content: $(this).find('.full-post').html(),
+                        style: 'codaStyle',
+                        position: {
+                            corner: {
+                                tooltip: cornerToolTip,
+                                target: cornerTarget
+                            }
+                        }
+                    });
+                });
+            });
+        }
+
+
+        function replaceHelper(str) {
+
+            str = str.replace('&#243;', 'ó').replace('&oacute;', 'ó');
+            str = str.replace('&amp;', '&');
+            str = str.replace('&nbsp;', ' ').replace('&#160;', ' ');
+            str = str.replace('&#8211;', '-').replace('&ndash;', "-");
+            str = str.replace('&ldquo;', '"').replace('&rdquo;', '"');
+            str = str.replace('&#8217;', "'").replace('&rsquo;', "'");
+            str = str.replace('&lt;', '<').replace('&gt;', '>');
+            return str;
+        }
+
+        function changeSort(elem) {
+
+            if ($(elem).text() == 'sortuj po tytule') {
+                $(elem).text('sortuj po dacie');
+                $.cookie('sortFunName', 'sortByTitle', { expires: 350 });
+                $.cookie('sortFunText', 'sortuj po dacie', { expires: 350 });
+            } else {
+                $(elem).text('sortuj po tytule');
+                $.cookie('sortFunName', 'sortByDate', { expires: 350 });
+                $.cookie('sortFunText', 'sortuj po tytule', { expires: 350 });
+            }
+
+            allDone();
+        }
+
+        function initialize() {
+
+            // check if user is using opera
+            if ($.browser.opera) {
+                $('.share-facebook').css('margin-left', '110px');
+            }
+
+            // nasty workaround for tweetme button on IE
+            var theframes = document.getElementsByTagName('iframe');
+            for (var i = 0; i < theframes.length; i++) {
+                theframes[i].setAttribute("allowTransparency", "true");
+            }
+
+            try {
+                var v = $.cookie('sortFunText');
+                if (v.length != 0 && v != 'undefined') {
+                    $('#sortBy').text(v);
+                }
+                
+                // sortuj po dacie
+            } catch (err) { }
+
+           // get all feeds at once
+           for (var feedId in testFeeds) {
+               var rssFeed = new google.feeds.Feed(testFeeds[feedId]);
+               callbackTrack.queue = callbackTrack.queue || [false];
+               rssFeed.load(callbackTrack);
+           }
+
+            allDone();
+        }
+
+        function doSort() {
+            try {
+                var funName = $.cookie('sortFunName');
+                if (funName.length > 0 && funName != 'undefined') {
+                    // alert(funName);
+                    eval('data.feeds.sort(' + funName + ');');
+                }
+            } catch (err) {
+                data.feeds.sort(sortByDate);
+            }
+        }
+
+        function start() {
+            $.ajax({
+                url : '/actualfeeds',
+                type : 'get',
+                success : function (data){
+                    testFeeds = data.value;
+                    initialize();
+                }
+            });
+        }
+
+        google.setOnLoadCallback(start);
