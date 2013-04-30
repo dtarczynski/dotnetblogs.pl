158cb9c2268c763c3500f35ea5eb2bae4945e785
LaMik <michal.franc@live.com>
2013-04-30 18:51:37 +0200

    moved all the files to root directory

--- db/init_data.js
+++ db/init_data.js
@@ -55,7 +55,7 @@
 ];
 
 /*var nano = require('nano')('http://localhost:5984');*/
-var nano = require('nano')('http://mfranc.iriscouch.com/');
+var nano = require('nano')('https://mfranc.iriscouch.com:6984');
 
 nano.db.destroy('dotnetblogs', function() {
   // create a new database
