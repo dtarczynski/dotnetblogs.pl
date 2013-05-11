45b9b061a21e379743b5e81595d79a821ad7c925
michal-franc <michal.franc@live.com>
2013-05-11 21:43:25 +0200

    restyled success and error messages, changes in localziation

--- custom_modules/netblogs/localization.js
+++ custom_modules/netblogs/localization.js
@@ -1,4 +1,4 @@
 module.exports.UrlNotEmpty = 'Pole adres url nie może być puste.';
 module.exports.UrlExists = 'Feed o podanym url juz&& istnieje.';
-module.exports.OperationSuccessfull = 'OK';
-module.exports.OperationFailed = 'Oops, Epic fail';
+module.exports.OperationSuccessfull = 'Yupii, nowy feed';
+module.exports.OperationFailed = 'Oops, no to mamy problem ;(';
